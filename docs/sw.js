14ee036c6dd58cb326d17275bf26392f644a6198
mgechev <mgechev@gmail.com>
2019-06-14 21:53:17 +0000

    Travis CI deployment

--- docs/sw.js
+++ docs/sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8e301002935589b400d1.js"
+    "url": "webpack-runtime-e6b9b4e3e6d5a3630a06.js"
   },
   {
     "url": "app.ea9621e90a4876e3ab3c.css"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "47105714e9430b90450957c42589f984"
+    "revision": "2e99d4eb3f12240edb3091e3e5ce948b"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b68ccb1b918e6c4fdd32f24de8ffff51"
+    "revision": "8d11144ffd34839209b1d66bf5850c78"
   },
   {
     "url": "component---src-pages-index-js.6c7a1760c6509a1091d6.css"
