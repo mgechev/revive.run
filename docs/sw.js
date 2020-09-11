fc2c4fd7a140902ac2508fc885f69a655c7c1fe5
mgechev <mgechev@gmail.com>
2020-09-11 20:09:21 +0000

    Travis CI deployment

--- docs/sw.js
+++ docs/sw.js
@@ -35,14 +35,14 @@ self.__precacheManifest = [
     "url": "styles-1047e3a8298aa65e6450.js"
   },
   {
-    "url": "app-be35552520e650f2de9a.js"
+    "url": "app-cd97f7380f9d63fb0d56.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-e4a0773b2d517d6afd53.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c6bc6635a555f64d46b9ae7760f1677a"
+    "revision": "0b2728d281e2199d0b0e411ec9a407ac"
   },
   {
     "url": "component---src-pages-404-js-f00280c643910a9b363c.js"
@@ -52,7 +52,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "f31d8fbddfb7c2a5fd5947d931dbc5f2"
+    "revision": "2258d8eeb71a78c715b7445590c55dba"
   },
   {
     "url": "manifest.webmanifest",
