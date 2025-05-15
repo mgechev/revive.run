f041ee20283b50f5443726a1007ceb57ff2578f5
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-05-15 20:27:57 +0300

    docs: update link to Go reference (#71)

--- src/components/docs-layout/index.js
+++ src/components/docs-layout/index.js
@@ -101,7 +101,7 @@ class App extends React.Component {
         </section>
         <section>
           <header>
-            <a href="https://godoc.org/github.com/mgechev/revive">API</a>
+            <a href="https://pkg.go.dev/github.com/mgechev/revive">API</a>
           </header>
         </section>
       </>
